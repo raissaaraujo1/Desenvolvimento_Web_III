0f38ab36478785a32af24a3c047e2d91f8ce569c
Raissa Araujo <raissaa520@gmail.com>
2025-03-20 12:07:20 -0300

    Reorganizando

---
+++ ConexaoMongo/PetMongoDB/PetMongoDB/Data/PetMongoDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetMongoDB.Models;
+
+namespace PetMongoDB.Data
+{
+    public class PetMongoDBContext : DbContext
+    {
+        public PetMongoDBContext (DbContextOptions<PetMongoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PetMongoDB.Models.Pet> Pet { get; set; } = default!;
+    }
+}
---
+++ ConexaoMongo/PetMongoDB/PetMongoDB/Models/ContextMongoDB.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace PetMongoDB.Models
+{
+    public class ContextMongoDB
+    {
+        public static string? ConnectionString { get; set; }
+        public static string? Database { get; set; }
+        //camada de segurança (true or false) por isso é booleano
+        public static bool IsSSL { get; set; }
+        public IMongoDatabase _database { get; }
+
+        static ContextMongoDB()
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+        }
+        public ContextMongoDB()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+                if(IsSSL)
+                {
+                    //verificação
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                }
+                //instancinado um objeto que será criado nessa váriavel
+                var mongoCliente = new MongoClient(settings);
+                _database = mongoCliente.GetDatabase(Database);
+
+            }
+            //criando uma excessão genérica
+            catch(Exception)
+            {
+                throw new Exception("Não foi possível conectar ao MongoDB");
+
+            }
+        }//conexão mongo
+
+        //criando a classe Pet no Mongo
+        public IMongoCollection <Pet> Pet
+        {
+             get
+            {
+                return _database.GetCollection<Pet>("Pet");
+            }
+        }
+
+    }//fim classe
+
+}//fim namespace
